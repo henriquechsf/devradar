9f8ec08da132bd335e43b25f15b4e149c3d1c9a4
Carlos Henrique <ch.ferreir@hotmail.com>
2020-01-16 01:11:04 -0300

    add rota para listar devs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,13 @@ const DevController = require('./controllers/DevController')
 
 const routes = Router()
 
+// index -> listar registros
+// show -> mostrar único registro
+// store -> criar registro
+// update -> alterar registro
+// destroy -> excluir registro
+
+routes.get('/devs', DevController.index)
 routes.post('/devs', DevController.store)
 
 module.exports = routes
