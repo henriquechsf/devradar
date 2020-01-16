196f5f4cc02ea55427a89c3be07145c1c7b74560
Carlos Henrique <ch.ferreir@hotmail.com>
2020-01-16 12:54:52 -0300

    estilo css no Main, captura da local. do browser

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,32 @@
+import React, { useEffect, useState } from 'react';
+
 import './global.css'
 import './App.css'
 import './Sidebar.css'
-
-import React from 'react';
+import './Main.css'
 
 function App() {
+  // estado do componente
+  const [latitude, setLatitude] = useState('')
+  const [longitude, setLongitude] = useState('')
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //console.log(position)
+        const { latitude, longitude } = position.coords
+
+        setLatitude(latitude)
+        setLongitude(longitude)
+      },
+      (err) => {
+        console.log(err)
+      },
+      {
+        timeout: 30000
+      }
+    )
+  }, [])
   
   return (
     <div id="app">
@@ -24,19 +46,84 @@ function App() {
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <input 
+                type="number" 
+                name="latitude" 
+                id="latitude" 
+                required 
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
+              <input 
+                type="number" 
+                name="longitude" 
+                id="longitude" 
+                required 
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              />
             </div>
           </div>
 
           <button type="submit">Salvar</button>
         </form>
       </aside>
-      <main></main>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/47280581?s=460&v=4" alt="Henrique Ferreira"/>
+              <div className="user-info">
+                <strong>Carlos Henrique</strong>
+                <span>React JS, Node JS</span>
+              </div>
+            </header>
+            <p>Web Development Student</p>
+            <a href="https://github.com/henriquechsf">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/47280581?s=460&v=4" alt="Henrique Ferreira"/>
+              <div className="user-info">
+                <strong>Carlos Henrique</strong>
+                <span>React JS, Node JS</span>
+              </div>
+            </header>
+            <p>Web Development Student</p>
+            <a href="https://github.com/henriquechsf">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/47280581?s=460&v=4" alt="Henrique Ferreira"/>
+              <div className="user-info">
+                <strong>Carlos Henrique</strong>
+                <span>React JS, Node JS</span>
+              </div>
+            </header>
+            <p>Web Development Student</p>
+            <a href="https://github.com/henriquechsf">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/47280581?s=460&v=4" alt="Henrique Ferreira"/>
+              <div className="user-info">
+                <strong>Carlos Henrique</strong>
+                <span>React JS, Node JS</span>
+              </div>
+            </header>
+            <p>Web Development Student</p>
+            <a href="https://github.com/henriquechsf">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
